b0f7015c20f8ba95f4c7085d1c0c99c24dde51fb
jmaerckaert <jmaerckaert@gmail.com>
2025-07-16 13:56:15 +0200

    correct entity failover

--- web/modules/custom/failover_log/src/FailoverLogListBuilder.php
+++ web/modules/custom/failover_log/src/FailoverLogListBuilder.php
@@ -4,13 +4,49 @@ declare(strict_types=1);
 
 namespace Drupal\failover_log;
 
+use Drupal\Core\Datetime\DateFormatterInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 
 /**
  * Construit la liste des entités Failover Log.
  */
 class FailoverLogListBuilder extends EntityListBuilder {
+
+    /**
+     * Le service de formatage des dates.
+     *
+     * @var \Drupal\Core\Datetime\DateFormatterInterface
+     */
+    protected $dateFormatter;
+
+    /**
+     * Construit le list builder.
+     *
+     * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+     *   Le type d'entité.
+     * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+     *   Le stockage de l'entité.
+     * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+     *   Le service de formatage des dates.
+     */
+    public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, DateFormatterInterface $date_formatter) {
+        parent::__construct($entity_type, $storage);
+        $this->dateFormatter = $date_formatter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function createInstance(\Symfony\Component\DependencyInjection\ContainerInterface $container, EntityTypeInterface $entity_type) {
+        return new static(
+            $entity_type,
+            $container->get('entity_type.manager')->getStorage($entity_type->id()),
+            $container->get('date.formatter')
+        );
+    }
 
     /**
      * {@inheritdoc}
@@ -21,7 +57,7 @@ class FailoverLogListBuilder extends EntityListBuilder {
                 'interface' => $this->t('Interface'),
                 'interface_label' => $this->t('Libellé de l’interface'),
                 'status' => $this->t('Statut'),
-                'duration' => $this->t('Durée (ms)'),
+                'duration' => $this->t('Durée (min)'),
                 'created' => $this->t('Créé le'),
             ] + parent::buildHeader();
     }
@@ -30,13 +66,15 @@ class FailoverLogListBuilder extends EntityListBuilder {
      * {@inheritdoc}
      */
     public function buildRow(EntityInterface $entity): array {
-        return [
-                'message' => $entity->get('message')->value,
-                'interface' => $entity->get('interface')->value,
-                'interface_label' => $entity->get('interface_label')->value,
-                'status' => $entity->get('status')->value,
-                'duration' => $entity->get('duration')->value,
-                'created' => $entity->get('created')->view(['type' => 'timestamp']),
-            ] + parent::buildRow($entity);
+        $row = [
+            'message' => $entity->get('message')->value ?? '',
+            'interface' => $entity->get('interface')->value ?? '',
+            'interface_label' => $entity->get('interface_label')->value ?? '',
+            'status' => $entity->get('status')->value ?? '',
+            'duration' => $entity->get('duration')->value ? $entity->get('duration')->value . ' min' : '',
+            'created' => $this->dateFormatter->format($entity->get('created')->value, 'medium'),
+        ];
+
+        return $row + parent::buildRow($entity);
     }
 }
